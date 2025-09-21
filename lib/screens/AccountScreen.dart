debae9793d602f53f2f534534897af1b9ad75bee
T1Keria <101090463+T1Keria@users.noreply.github.com>
2025-09-21 23:09:42 +0900

    first commit

--- lib/screens/AccountScreen.dart
+++ lib/screens/AccountScreen.dart
@@ -7,7 +7,7 @@ class AccountScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("가계부 화면"),
+        title: const Text("가계부 화면1"),
       ),
       body: const Center(child: Text("여기는 가계부 화면")),
     );
